81ef1bd067a99fbca42f2318c18e0659c8df3d21
Chengqian Hu <aubrey1998@outlook.com>
2025-06-19 00:22:35 +1000

    P2P-9 add necessary cmake configurations to top-level CMakeList and hello world in main to build executable

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+// Necessary header files for input output functions
+#include <iostream>
+#include "p2p-lan-share/main.h"
+
+using namespace std;
+
+// main() function: where the execution of
+// C++ program begins
+int main() {
+  
+    // This statement prints "Hello World"
+    cout << "Hello World";
+
+    return 0;
+}
